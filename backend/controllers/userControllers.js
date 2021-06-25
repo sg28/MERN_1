e52fcce91ff291cf36486726dfccac09e0a4a6a4
root <root@US-SGHOSH.fortinet-us.com>
2021-06-24 23:33:31 -0700

    backend/connecting and testing api call with mongoDb

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,31 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, pic } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exist");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Error Occured");
+  }
+});
+module.exports = { registerUser };
--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,28 @@ const express = require("express");
 const notes = require("./data/notes");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleWare");
 
 const app = express();
 dotenv.config();
 connectDB();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Api is running");
-});
+// app.get("/", (req, res) => {
+//   res.send("Api is running");
+// });
 app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
-app.get("/api/notes/:id", (req, res) => {
-  const note = notes.find((n) => n._id === req.params.id);
-  res.send(note);
-});
+// app.get("/api/notes/:id", (req, res) => {
+//   const note = notes.find((n) => n._id === req.params.id);
+//   res.send(note);
+// });
+
+app.use("/api/users", userRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(` Server started on port ${PORT} `));
